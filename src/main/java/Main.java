f384133b408adda2f49e40ebffdefedd327e73a4
Vhreya <va.au@gmx.at>
2020-11-05 14:20:44 +0100

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        Lens lens1 = new Lens(32, 35);
+        Lens lens2 = new Lens(31, 32);
+
+        Camera camera1 = new Camera("nikon", 24.2, 1037000, true, lens1);
+        Camera camera2 = new Camera("blubb", 32.1, 900000,false, lens1);
+        
+        camera1.setLens(lens2);
+
+        System.out.println(camera1);
+        System.out.println("How many lenses do we have: " + Lens.getCOUNTERLENSES());
+        System.out.println("How many cameras do we have: " + Camera.getCOUNTER());
     }
 
 }
---
+++ src/main/java/TestCases.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class TestCases {
+
+    Lens lensTest = new Lens(30, 32);
+    Camera cameraTest = new Camera("test", 1234, 12345, true, lensTest);
+    Lens lensTest2 = new Lens(20, 25);
+
+    public TestCases() throws Exception {
+    }
+
+    @Test
+    public void testSetLowerMaxFocalLengthThanMinFocalLength(){
+        assertThrows (RuntimeException.class, () -> lensTest.setMaxFocalLength(29));
+        assertEquals(32, lensTest.getMaxFocalLength());
+    }
+
+    @Test
+    public void testSwitchLenses(){
+        cameraTest.setLens(lensTest2);
+        assertEquals(lensTest2, cameraTest.getLens());
+    }
+
+    @Test
+    public void testIfCounterVariableForCameraWorks(){
+        assertEquals(3, Camera.getCOUNTER());
+        Camera Test2 = new Camera("bla", 123, 1234, true, lensTest);
+        assertEquals(4, Camera.getCOUNTER());
+    }
+
+    @Test
+    public void testIfCounterVariableForLensWorks() throws Exception {
+        assertEquals(4, Lens.getCOUNTERLENSES());
+        Lens test3 = new Lens(20, 25);
+    }
+}
